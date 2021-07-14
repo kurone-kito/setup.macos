3bd1b75f2816639cea85249a2a3cbc0ea8dc7b6c
kurone-kito <krone@kit.black>
2021-07-14 13:19:17 +0900

    feat: improved the Homebrew installation

--- .setup.brew.sh
+++ .setup.brew.sh
@@ -6,17 +6,37 @@ cd $(dirname $0)
 
 . .setup.lib.sh
 
-if [ $(which brew | wc -l) -eq 0 ]; then
+if type -a brew >& /dev/null
+then
+  log_info 'Updating the Homebrew CLI.'
+
+  brew upgrade
+else
   log_info 'Installing the Homebrew CLI.'
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   source .zsh.d/homebrew
 fi
 
+if ! type -a mas >& /dev/null
+then
+  log_info 'Installing the minimum apps required to install bundles'
+
+  brew install mas
+fi
+
+if ! mas account > /dev/null
+then
+  say_warn 'Please open App Store and sign in using your Apple ID ...'
+  until mas account > /dev/null
+  do
+    sleep 3
+  done
+fi
+
 log_info 'Installing the Homebrew bundles.'
 
 brew bundle --file 'Brewfile'
-brew upgrade
 brew cleanup
-brew autoupdate delete
+brew autoupdate delete > /dev/null
 brew autoupdate start
