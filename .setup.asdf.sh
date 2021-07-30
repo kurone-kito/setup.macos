bafcb848bcff42da2fa281db9985c54d32b28d90
kurone-kito <krone@kit.black>
2021-07-30 13:20:03 +0900

    feat: added the asdf and asdf-nodejs configurations

---
+++ .setup.asdf.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# vim: set ft=sh :
+
+set -eu
+cd "$(dirname "$0")"
+
+. .setup.lib.sh
+
+if ! type -a asdf > /dev/null 2>&1
+then
+  log_notice 'Skipped the ASDF updating because it is not found on this system.'
+  exit
+fi
+
+log_info 'Setup the asdf-nodejs.'
+. .zsh.d/z-asdf
+
+install_plugin() {
+  if [[ "$(asdf plugin list | grep -c "${1}")" -eq 0 ]]
+  then
+    asdf plugin add "$@"
+  fi
+}
+
+install_plugin nodejs
