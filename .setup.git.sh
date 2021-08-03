46cd42ccbf988e47cbef15f99a6b168949a7aa5a
kurone-kito <krone@kit.black>
2021-08-03 13:58:20 +0900

    fix: fixed a bug the .zshrc overwrite by default Prezto settings

--- .setup.git.sh
+++ .setup.git.sh
@@ -14,9 +14,10 @@ MY_REPOS="${HOME}/src/my"
 DOTFILES="${MY_REPOS}/dotfiles"
 mkdir -p "${MY_REPOS}"
 github_clone_if_not_exists kurone-kito/dotfiles "${DOTFILES}"
-"${DOTFILES}/setup"
 
 for rcfile in $(find "${ZDOTDIR:-$HOME}"/.zprezto/runcoms -type f -name 'z*')
 do
   ln -snf "${rcfile}" "${ZDOTDIR:-$HOME}/.${rcfile##*/}"
 done
+
+"${DOTFILES}/setup"
