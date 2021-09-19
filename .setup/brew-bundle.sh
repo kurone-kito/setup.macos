56281b1340b5a55dc605ecde6aa9f76dfb82c13a
kurone-kito <krone@kit.black>
2021-09-20 08:45:54 +0900

    feat: added the pre-sudo

--- .setup/brew-bundle.sh
+++ .setup/brew-bundle.sh
@@ -9,6 +9,9 @@ cd "$(dirname "$0")"
 log_info 'Installing the Homebrew bundles.'
 log_warn 'DO NOT CLOSE this terminal until instructed to do so. During installation, it may ask you to enter your password if necessary.'
 
+sudo -v
+while true; do sleep 50; sudo -n true; kill -0 "$$" || exit; done 2>/dev/null &
+
 mkdir -p "${HOME}/Library/LaunchAgents"
 brew tap 'homebrew/autoupdate'
 brew autoupdate delete > /dev/null 2>&1 || true
