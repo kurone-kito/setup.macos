5f3800a8892224ebcbe26b3e88c288d328515729
kurone-kito <krone@kit.black>
2021-11-10 13:36:02 +0900

    fix: fixed a bug that caused the setup to fail to proceed on macOS Monterey

--- .setup/brew-pre-bundle.sh
+++ .setup/brew-pre-bundle.sh
@@ -8,15 +8,18 @@ cd "$(dirname "$0")"
 
 log_info 'Installing the minimum apps required to install bundles'
 
-brew install mas 2> /dev/null
+brew install coreutils mas 2> /dev/null
 
 if ! mas account > /dev/null
 then
   open '/System/Applications/App Store.app'
-  say_warn 'Please open App Store and sign in using your Apple ID ...'
+  say_warn 'Please open App Store and sign in using your Apple ID. The setup will automatically recognize your sign-in and go to the next step, but it may not detect your sign-in correctly on the macOS Monterey. In that case, you can also force it to go to the next step by pressing the [ENTER] key.'
   until mas account > /dev/null
   do
-    sleep 3
+    if timeout --foreground 7s read
+    then
+      break
+    fi
   done
 fi
 
