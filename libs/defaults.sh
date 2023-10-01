2cef0e5f8939fd6e25ef8a764e4e26422dd7f4e5
kurone-kito <krone@kit.black>
2023-10-01 11:05:30 +0900

    feat: improved the defaults configuration

--- libs/defaults.sh
+++ libs/defaults.sh
@@ -12,7 +12,7 @@ osascript -e 'tell application "System Preferences" to quit'
 # See: https://macos-defaults.com/
 
 # Activity Monitor ========================================================
-# Visualize CPU usage in the Dock icon
+# Visualize CPU usage in the Dock icon.
 defaults write com.apple.ActivityMonitor IconType -int 6
 
 killall 'Activity Monitor' 2> /dev/null || true
@@ -38,57 +38,66 @@ FINDER_PLIST="${HOME}/Library/Preferences/com.apple.finder.plist"
 /usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:showItemInfo true' "${FINDER_PLIST}" || \
 /usr/libexec/PlistBuddy -c 'Add :DesktopViewSettings:IconViewSettings:showItemInfo bool true' "${FINDER_PLIST}"
 
+# Always display folders at the top.
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+
 killall Finder 2> /dev/null || true
 
-# Font Book ========================================================
+# Font Book ===============================================================
 defaults write com.apple.FontBook FBResolveByMovingToTrashKey -bool true
 
 killall 'Font Book' 2> /dev/null || true
 
 # Dock & Mission Control ==================================================
-# Enable spring loading for all Dock items
+# Enable spring loading for all Dock items.
 defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
 
-# Mission Control
+# Group windows by application on Mission Control.
 defaults write com.apple.dock expose-group-apps -bool true
 
-# Enable magnification on mouse-over
+# Enable magnification on mouse-over.
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock largesize -int 128
 
-# Minimize animation effect
+# Minimize animation effect.
 defaults write com.apple.dock mineffect -string suck
 
 killall Dock 2> /dev/null || true
 
 # Finder ==================================================================
-# Avoid creating the “.DS_Store” files on network volumes
+# Avoid creating the “.DS_Store” files on network volumes.
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Avoid creating the “.DS_Store” on USB volumes
+# Avoid creating the “.DS_Store” on USB volumes.
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Switch the default Finder view style to Column view
+# Switch the default Finder view style to Column view.
 defaults write com.apple.Finder FXPreferredViewStyle clmv
 
-# Remove item from trash after 30 days
+# Remove item from trash after 30 days.
 defaults write com.apple.finder FXRemoveOldTrashItems -bool true
 
+# Show the HOME folder on first by the new Finder windows.
 defaults write com.apple.finder NewWindowTarget -string 'PfHm'
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
 
-# Always display folders at the top
+# Always display folders at the top.
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
-defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
 
-# Show all files extensions
+# Show path bar in the bottom of the Finder windows.
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show folder icon before title in the title bar.
+defaults write com.apple.universalaccess showWindowTitlebarIcons -bool true
+
+# Show all files extensions.
 defaults write -g AppleShowAllExtensions -bool true
 
-# Expand the save panel in default
+# Expand the save panel in default.
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
 
-# Set sidebar icon size
+# Set sidebar icon size.
 defaults write -g NSTableViewDefaultSizeMode -int 3
 
 killall Finder 2> /dev/null || true
@@ -114,14 +123,21 @@ killall Finder 2> /dev/null || true
 # Menubar =================================================================
 defaults write com.apple.controlcenter 'NSStatusItem Visible Bluetooth' -bool false
 defaults write com.apple.controlcenter 'NSStatusItem Visible WiFi' -bool false
+
+# The time separator flashes every second.
 defaults write com.apple.menuextra.clock FlashDateSeparators -bool true
+
 defaults write com.apple.menuextra.clock Show24Hour -bool true
 defaults write com.apple.Siri StatusMenuVisible -bool false
 
 killall SystemUIServer 2> /dev/null || true
 
-# Mouse ================================================================
+# Mouse / Trackpad ========================================================
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode -string TwoButton
+
+# Click weight to firm.
+defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 2
+
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string TwoButton
 
 killall Finder 2> /dev/null || true
@@ -137,7 +153,7 @@ killall cfprefsd 2> /dev/null || true
 defaults write com.apple.TextEdit CheckGrammarWithSpelling -bool true
 defaults write com.apple.TextEdit RichText -bool false
 
-# Using XHTML 1.0 Strict
+# Using XHTML 1.0 Strict.
 defaults write com.apple.TextEdit UseTransitionalDocType -bool false
 defaults write com.apple.TextEdit UseXHTMLDocType -bool true
 
